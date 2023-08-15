adb2a3244766b307f8957c5aa22273cfd410fcd3
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2023-08-14 21:59:40 -0500

    taller completo
    creo el taller completo

---
+++ Taller_Santiago_ Jaimes/model/modelStudents.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        "name":"Andres Felipe",
+        "lastName":"Nieto Jimenez",
+        "age":"25"
+    },
+    {
+        "name":"Andres Felipe",
+        "lastName":"Mosquera Hernandez",
+        "age":"29"
+    },
+    {
+        "name":"Sebastian",
+        "lastName":"Ballen Loaiza",
+        "age":"20"
+    },
+    {
+        "name":"Jose Felipe",
+        "lastName":"Carvajal Campos",
+        "age":"23"
+    }
+];
+
+export {
+    data as students
+}
